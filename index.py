54ad4ab425b5b7382f6a2786088b90bb27260907
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2020-08-09 19:08:34 +0530

    Adding Database

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/blog"
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    '''sr_no	name	email	phone	message	'''
+    sr_no = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(200), nullable=True)
 
 @app.route("/") 
 def home(): 
@@ -10,8 +22,18 @@ def about():
     name = "Mayank Khurmai"
     return render_template("about.html", fname=name) 
 
-@app.route("/contact") 
-def contact(): 
-    return render_template("contact.html")
+@app.route("/contact", methods = ['GET', 'POST'])
+def contact():
+    if(request.method=='POST'):
+        ''' Add entry to the database'''
+        ''' sr_no	name	email	phone	message	'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name=name, email = email, phone = phone, message = message)
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html')
     
 app.run(debug=True)
