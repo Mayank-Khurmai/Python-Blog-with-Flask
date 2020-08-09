0767909d1ee1a2d108155be2d936648a60340ee8
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2020-08-09 11:51:25 +0530

    Jinga Technique

--- index.py
+++ index.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__) 
 
 @app.route("/") 
-def hello(): 
-    return render_template("index.html")
+def home(): 
+    return render_template("home.html")
 
 @app.route("/about") 
-def harry(): 
+def about(): 
     name = "Mayank Khurmai"
     return render_template("about.html", fname=name) 
+
+@app.route("/contact") 
+def contact(): 
+    return render_template("contact.html")
     
 app.run(debug=True)
